24658aaf07d7b751f2864a5cea85e9952defb5ad
Pine Mizune <pinemz@gmail.com>
2019-05-25 16:14:06 +0900

    docs: update copyright

--- lib/Mac/OSVersion/Lite.pm
+++ lib/Mac/OSVersion/Lite.pm
@@ -163,7 +163,7 @@ Convert a C<SemVer::V2::Strict> instance to string.
 
 The MIT License (MIT)
 
-Copyright (c) 2017 Pine Mizune
+Copyright (c) 2017-2019 Pine Mizune
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
